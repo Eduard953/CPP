6ba9f763bcbf7f7031d36f7bc4b858c4c46b9dac
ebeiline <ebeiline@students.42wolfsburg.de>
2022-05-02 12:24:52 +0300

    ex02 done

---
+++ cp01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/01 19:49:14 by ebeiline          #+#    #+#             */
+/*   Updated: 2022/05/02 12:22:59 by ebeiline         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringRef = string;
+
+	std::cout << "memory address of the string variable: " << &string << std::endl;
+	std::cout << "memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "memory address held by stringREF: " << &stringRef << std::endl;
+	
+	std::cout << "value of the string variable: " << string << std::endl;
+	std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF: " << stringRef << std::endl;
+}
