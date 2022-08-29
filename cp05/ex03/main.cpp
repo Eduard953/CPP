b2222434261f34c3a587311e636f83a38162f83e
ebeiline <ebeiline@students.42wolfsburg.de>
2022-08-29 14:42:14 +0200

    added readability to main

--- cp05/ex03/main.cpp
+++ cp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebeiline <ebeiline@42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:19:11 by ebeiline          #+#    #+#             */
-/*   Updated: 2022/05/10 14:03:02 by ebeiline         ###   ########.fr       */
+/*   Updated: 2022/08/29 14:41:12 by ebeiline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,27 @@ int main(void)
 	RobotomyRequestForm rob;
 	Intern intern;
 	
-
+	std::cout << std::endl;
+	
 	Form *test1 = intern.makeForm("Presidential Pardon Form", "house");
 	Form *test2 = intern.makeForm("Robotomy Request Form", "house");
 	Form *test3 = intern.makeForm("Shrubbery Form", "house");
 	Form *test4 = intern.makeForm("undefined", "house");
 	
+	std::cout << std::endl;
+	
 	zel.signForm(*test1);
 	zel.signForm(*test2);
 	zel.signForm(*test3);
 
+	std::cout << std::endl;
+	
 	test1->execute(zel);
 	test2->execute(zel);
 	test3->execute(zel);
 
+	std::cout << std::endl;
+	
 	delete test1;
 	delete test2;
 	delete test3;
